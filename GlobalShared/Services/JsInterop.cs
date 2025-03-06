4e2bc1c9cda126c56711f9bc8e5994909c471984
moemyint6595 <108658301+moemyint6595@users.noreply.github.com>
2025-03-06 21:18:48 +0630

    Clean Project

--- GlobalShared/Services/JsInterop.cs
+++ GlobalShared/Services/JsInterop.cs
@@ -4,27 +4,33 @@ namespace GlobalShared.Services
 {
     public class JsInterop : IAsyncDisposable
     {
-        private readonly Lazy<Task<IJSObjectReference>> threeRenderModule;
+        //private readonly Lazy<Task<IJSObjectReference>> threeRenderModule;
 
         public JsInterop(IJSRuntime jsRuntime)
         {
-            threeRenderModule = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
-              "import", "./Render.js").AsTask());
+            //threeRenderModule = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+            //  "import", "./Render.js").AsTask());
 
         }
-        public async ValueTask CreateBoxModal(string target)
-        {
-            var module = await threeRenderModule.Value;
-            await module.InvokeVoidAsync("CreateBoxModal", target);
-        }
+        //public async ValueTask CreateBoxModal(string target)
+        //{
+        //    var module = await threeRenderModule.Value;
+        //    await module.InvokeVoidAsync("CreateBoxModal", target);
+        //}
+
+        //public async ValueTask CreateCarModal(string target)
+        //{
+        //    var module = await threeRenderModule.Value;
+        //    await module.InvokeVoidAsync("LoadCarModal", target);
+        //}
 
         public async ValueTask DisposeAsync()
         {
-            if (threeRenderModule.IsValueCreated)
-            {
-                var module = await threeRenderModule.Value;
-                await module.DisposeAsync();
-            }
+            //if (threeRenderModule.IsValueCreated)
+            //{
+            //    var module = await threeRenderModule.Value;
+            //    await module.DisposeAsync();
+            //}
         }
     }
 }
